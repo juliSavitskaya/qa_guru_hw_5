1e0c8ebf7d39905ff53c039b493adcc6ae0c4b46
Yuliya Savickaya <ysavickaya@marfa-tech.com>
2025-06-04 12:54:22 +0300

    add tests

---
+++ test_hw.py
@@ -0,0 +1,94 @@
+from datetime import time
+
+
+def test_dark_theme_by_time():
+    """
+    Протестируйте правильность переключения темной темы на сайте в зависимости от времени
+    """
+    current_time = time(hour=23)
+    # Проверяем, входит ли текущее время в ночной интервал
+    if current_time >= time(22, 0) or current_time <= time(6, 0):
+        is_dark_theme = True
+    else:
+        is_dark_theme = False
+
+    assert is_dark_theme is True
+
+
+def test_dark_theme_by_time_and_user_choice():
+    """
+    Протестируйте правильность переключения темной темы на сайте
+    в зависимости от времени и выбора пользователя
+    dark_theme_enabled_by_user = True - Темная тема включена
+    dark_theme_enabled_by_user = False - Темная тема выключена
+    dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
+    """
+    current_time = time(hour=16)
+    dark_theme_enabled_by_user = True
+
+    if dark_theme_enabled_by_user is not None:
+        is_dark_theme = dark_theme_enabled_by_user
+    else:
+        if current_time >= time(22, 0) or current_time <= time(6, 0):
+            is_dark_theme = True
+        else:
+            is_dark_theme = False
+
+    assert is_dark_theme is True
+
+
+def test_find_suitable_user():
+    """
+    Найдите нужного пользователя по условиям в списке пользователей
+    """
+    users = [
+        {"name": "Oleg", "age": 32},
+        {"name": "Sergey", "age": 24},
+        {"name": "Stanislav", "age": 15},
+        {"name": "Olga", "age": 45},
+        {"name": "Maria", "age": 18},
+    ]
+
+    # Поиск пользователя с именем "Olga" с помощью генератора списка
+    suitable_users = [user for user in users if user["name"] == "Olga"]
+    # Поиск пользователя с именем "Olga" с помощью функции filter
+    # suitable_users = list(filter(lambda user: user["name"] == "Olga", users))
+
+    assert suitable_users == [{"name": "Olga", "age": 45}]
+
+    # Поиск всех пользователей младше 20 лет
+    suitable_users = [user for user in users if user["age"] < 20]
+    assert suitable_users == [
+        {"name": "Stanislav", "age": 15},
+        {"name": "Maria", "age": 18},
+    ]
+
+
+def print_readable_function_name(func, **kwargs):
+    readable_name = ' '.join(word.capitalize() for word in func.__name__.split('_'))
+    args_str = ', '.join(map(str, kwargs.values()))
+    result = f"{readable_name} [{args_str}]"
+    print(result)
+    return result
+
+
+def test_readable_function():
+    open_browser(browser_name="Chrome")
+    go_to_companyname_homepage(page_url="https://companyname.com")
+    find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
+
+
+def open_browser(browser_name):
+    actual_result = print_readable_function_name(open_browser, browser_name=browser_name)
+    assert actual_result == "Open Browser [Chrome]"
+
+
+def go_to_companyname_homepage(page_url):
+    actual_result = print_readable_function_name(go_to_companyname_homepage, page_url=page_url)
+    assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
+
+
+def find_registration_button_on_login_page(page_url, button_text):
+    actual_result = print_readable_function_name(find_registration_button_on_login_page,
+                                                 page_url=page_url, button_text=button_text)
+    assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
